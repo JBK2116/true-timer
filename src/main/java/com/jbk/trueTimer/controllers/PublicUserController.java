221e28c29504ba0cf47c214b2de9a3582d9fcd5b
jovkb <jovani.badinga7@gmail.com>
2025-09-12 22:56:32 -0400

    Added Proper Swagger Docs To Create User Endpoint

--- src/main/java/com/jbk/trueTimer/controllers/PublicUserController.java
+++ src/main/java/com/jbk/trueTimer/controllers/PublicUserController.java
@@ -2,6 +2,12 @@ package com.jbk.trueTimer.controllers;
 
 import com.jbk.trueTimer.dto.UserCreationDTO;
 import com.jbk.trueTimer.services.PublicUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "User Management", description = "Operations for managing users")
+
 public class PublicUserController {
 
     private final PublicUserService publicUserService;
@@ -20,6 +28,12 @@ public class PublicUserController {
     }
 
     @PostMapping("")
+    @Operation(summary = "Create User", description = "Create a user and return their unique token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User Created",
+                    content = @Content(examples = {@ExampleObject(value = "{\"token\": \"f47ac10b-58cc-4372-a567-0e02b2c3d479Retry\"}")})),
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                    content = @Content(examples = {@ExampleObject(value = "{'field name': 'error message'}")}))})
     public ResponseEntity<Void> createUser(@Valid @RequestBody UserCreationDTO dto) {
         System.out.println(dto.toString());
         return ResponseEntity.status(HttpStatus.CREATED).build();
