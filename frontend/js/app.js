f7fabf372d4ae2665b87de64c2b3b7b9e0cc1a6f
jovbk <jovani.badinga7@gmail.com>
2025-10-29 18:32:22 -0400

    Created current time display functionality and more -- READ COMMIT
    1. Current Time Display functionality is created
    2. IANA Timezone String is retrieved from user's browser
    3. Created extra helper functions.
    
    Check the git diff.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,15 @@
 /*
 Frontend entry point for this application
  */
+import * as utils from './utils.js';
 
+// ELEMENT DECLARATIONS
+
+function main() {
+    let timezone = utils.getUserIANAString();
+    console.log(`Timezone: ${timezone}`);
+    utils.initializePage(timezone);
+    utils.showNotificationDynamic("Application ready for use", 3);
+}
+
+document.addEventListener('DOMContentLoaded', main);
--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,3 +1,96 @@
 /*
 This files stores util functions used throughout the frontend
  */
+
+/**
+ * Purpose: Gets the IANA String from the user's browser.
+ * @example - If the user is in Toronto, IANA String = America/Toronto
+ *
+ * @returns {string} - User's `IANA String` if successful, else default to `UTC`
+ */
+export function getUserIANAString() {
+    try {
+        return Intl.DateTimeFormat().resolvedOptions().timeZone;
+    } catch (err) {
+        console.log(`Error retrieving user timezone. Defaulting to: UTC`);
+        console.log(`Error: ${err}`);
+    }
+}
+
+/** Purpose: Displays the notification modal using the provided message and for the provided duration
+ * @param {string} message Message to display
+ * @param {number} duration Duration to display modal (Seconds)
+ */
+export function showNotificationDynamic(message, duration) {
+    let notificationModal = createNotification(message);
+    if (!notificationModal) {
+        console.log("No notification modal found");
+        return;
+    }
+    notificationModal.classList.add("modal--show");
+    setTimeout(() => {
+        notificationModal.classList.remove("modal--show");
+    }, duration * 1000)
+}
+
+/** Purpose: Displays the notification modal using the provided message. 
+ * Notification modal remains displayed until close button is clicked on it.
+ * @param {string} message Message to display
+ */
+export function showNotificationStatic(message) {
+    let notificationModal = createNotification(message);
+    if (!notificationModal) {
+        console.log("No notification modal found");
+        return;
+    }
+    notificationModal.classList.add("modal--show");
+}
+
+/**
+ * Purpose: Initializes all necessary components of the frontend
+ * @param {string} IANATimezone
+ */
+export function initializePage(IANATimezone) {
+    // NOTIFICATION MODAL EVENT HANDLER
+    let notificationModal = document.getElementById("notification-modal");
+    let closeButton = notificationModal.querySelector(".modal__close");
+    closeButton.addEventListener("click", function (event) {
+        notificationModal.classList.remove("modal--show");
+    })
+    // CURRENT TIME DISPLAY
+    let currentTimeDisplay = document.getElementById("current-time-display");
+    function updateTime() {
+        const now = new Date();
+        currentTimeDisplay.textContent = createTimeString(now, IANATimezone);
+    }
+    updateTime();
+    setInterval(updateTime, 1000);
+}
+
+/** Purpose: Creates the notification modal using the provided message.
+ * @param {string} message Message to set in the modal
+ */
+function createNotification(message) {
+    let notificationModal = document.getElementById("notification-modal");
+    let messageBody = document.getElementById("notification-message");
+    if (!notificationModal || !messageBody) {
+        return null;
+    }
+    messageBody.textContent = message;
+    return notificationModal;
+}
+
+/** Purpose: Creates a HH:MM:SS time string given the provided time and IANATimezone
+ * @param {Date} time Current Time Object
+ * @param {string} IANATimezone Timezone of current time
+ */
+function createTimeString(time, IANATimezone) {
+    return time.toLocaleTimeString("en-US", {
+        timeZone: IANATimezone,
+        hour12: false,   // 24-hour format
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+}
+
